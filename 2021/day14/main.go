75aca57e5952b30565e3a62e970affce91a95367
Neil Fenwick <neil.fenwick@gmail.com>
2022-04-09 16:48:45 +0100

    Day14 part 1

--- 2021/day14/main.go
+++ 2021/day14/main.go
@@ -1,8 +1,102 @@
 package main
 
-import "github.com/neilfenwick/advent-of-code/files"
+import (
+	"bufio"
+	"fmt"
+	"github.com/neilfenwick/advent-of-code/data/linkedList"
+	"github.com/neilfenwick/advent-of-code/files"
+	"io"
+	"log"
+	"math"
+	"strings"
+)
 
 func main() {
-	files.ReadInputStream()
+	file, err := files.ReadInputStream()
+	if err != nil {
+		log.Fatal("Could not open input for reading")
+	}
+	defer func(file io.ReadCloser) {
+		_ = file.Close()
+	}(file)
 
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	input := scanner.Text()
+	scanner.Scan()
+	polymerMap := buildPolymerMap(scanner)
+	min, max := countMinMaxElementOccurrences(input, polymerMap)
+	fmt.Printf("Quantities: most common %d, least common %d. Difference: %d\n", max, min, max-min)
+}
+
+func buildPolymerMap(scanner *bufio.Scanner) map[string]rune {
+	polymerMap := make(map[string]rune)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if len(line) == 0 {
+			continue
+		}
+		parts := strings.Split(line, " -> ")
+		polymerMap[parts[0]] = []rune(parts[1])[0]
+	}
+	return polymerMap
+}
+
+func countMinMaxElementOccurrences(input string, polymers map[string]rune) (int, int) {
+	elementsLinkedList := linkedList.NewRuneLinkedList([]rune(input))
+	for i := 0; i < 10; i++ {
+		expandElements(elementsLinkedList, polymers)
+	}
+	elementCounts := make(map[rune]int)
+	current := elementsLinkedList.Head
+	for {
+		count, _ := elementCounts[current.Value]
+		elementCounts[current.Value] = count + 1
+		if current.Next == nil {
+			break
+		}
+		current = current.Next
+	}
+	min, max := math.MaxInt, 0
+	for _, v := range elementCounts {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	return min, max
+}
+
+func expandElements(elementsList *linkedList.RuneLinkedList, polymers map[string]rune) {
+	currentElement := elementsList.Head
+	newElements := linkedList.NewRuneLinkedList([]rune{})
+	for {
+		if currentElement.Next == nil {
+			break
+		}
+		pair := []rune{currentElement.Value, currentElement.Next.Value}
+		newElementValue := polymers[string(pair)]
+		newElements.AppendValue(newElementValue)
+		currentElement = currentElement.Next
+	}
+	mergeLinkedLists(elementsList, newElements)
+}
+
+func mergeLinkedLists(first *linkedList.RuneLinkedList, second *linkedList.RuneLinkedList) {
+	currentElement := first.Head
+	currentSecondListElement := second.Head
+	var tempNext *linkedList.RuneLinkedListNode
+	for {
+		tempNext = currentElement.Next
+		if tempNext == nil || currentSecondListElement == nil {
+			return
+		}
+		currentElement.Next = currentSecondListElement
+		currentElement = currentSecondListElement
+		currentSecondListElement = currentSecondListElement.Next
+		currentElement.Next = tempNext
+		currentElement = tempNext
+	}
 }
---
+++ data/linkedList/runeLinkedList.go
@@ -0,0 +1,52 @@
+package linkedList
+
+type RuneLinkedList struct {
+	Head *RuneLinkedListNode
+	Tail *RuneLinkedListNode
+}
+
+type RuneLinkedListNode struct {
+	Value rune
+	Next  *RuneLinkedListNode
+}
+
+func NewRuneLinkedList(initialValues []rune) *RuneLinkedList {
+	var (
+		result   RuneLinkedList
+		previous *RuneLinkedListNode
+	)
+	for _, char := range initialValues {
+		if result.Head == nil {
+			result.Head = &RuneLinkedListNode{Value: char}
+			previous = result.Head
+			continue
+		}
+		previous.Next = &RuneLinkedListNode{Value: char}
+		result.Tail = previous.Next
+		previous = previous.Next
+	}
+	return &result
+}
+
+func (linkedList *RuneLinkedList) AppendValue(value rune) {
+	if linkedList.Head == nil {
+		linkedList.Head = &RuneLinkedListNode{Value: value}
+		linkedList.Tail = linkedList.Head
+		return
+	}
+	linkedList.Tail.Next = &RuneLinkedListNode{Value: value}
+	linkedList.Tail = linkedList.Tail.Next
+}
+
+func (linkedList *RuneLinkedList) String() string {
+	result := make([]rune, 0)
+	current := linkedList.Head
+	for {
+		result = append(result, current.Value)
+		if current.Next == nil {
+			break
+		}
+		current = current.Next
+	}
+	return string(result)
+}
