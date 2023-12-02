14ecce50e27d8d234d8a4b6bf086ed57964959e2
Neil Fenwick <neil.fenwick@gmail.com>
2023-12-02 15:06:00 +0000

    2023 day 1 part 2
    Naive approach by manually replacing text sequentially.

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"slices"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -23,39 +26,113 @@ func main() {
 		_ = file.Close()
 	}(file)
 
-	coords := parseCoordinates(file)
-    sum := sumCoords(coords)
-	fmt.Println(sum)
+	coordsPt1 := parseCoordinates(file, func(i string) string { return i })
+	sumPt1 := sumCoords(coordsPt1)
+	fmt.Printf("Part1 sum: %d\n", sumPt1)
+
+	file.Seek(0, 0)
+
+	coordsPt2 := parseCoordinates(file, sequentialReplaceNumberWords)
+	sumPt2 := sumCoords(coordsPt2)
+	fmt.Printf("Part2 sum: %d\n", sumPt2)
 }
 
-func parseCoordinates(reader io.Reader) []string {
+var wordToDigitMap = map[string]rune{
+	"one":   '1',
+	"two":   '2',
+	"three": '3',
+	"four":  '4',
+	"five":  '5',
+	"six":   '6',
+	"seven": '7',
+	"eight": '8',
+	"nine":  '9',
+}
+
+type digitParseFuncStrategy func(string) string
+
+func parseCoordinates(reader io.Reader, parser digitParseFuncStrategy) []string {
 	scanner := bufio.NewScanner(reader)
 	results := make([]string, 0)
 	for scanner.Scan() {
-		line := scanner.Text()
-		var digits string
-		for _, char := range line {
-			switch char {
-			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-				digits = string(char)
-				goto nextchar
-			}
+		line := parser(scanner.Text())
+		first, err := findFirstDigit(line)
+		if err != nil {
+			log.Println(err)
+			continue
 		}
 
-	nextchar:
-		for i := len(line) - 1; i >= 0; i-- {
-			switch line[i] {
-			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-				digits += string(line[i])
-				goto done
-			}
+		digits := string(first)
+		last, err := findLastDigit(line)
+		if err != nil {
+			log.Println(err)
+			continue
 		}
 
-	done:
+		digits += string(last)
 		results = append(results, digits)
 	}
 
 	return results
+}
+
+func sequentialReplaceNumberWords(line string) string {
+	var result, previous string
+	previous = ""
+    result = line
+	for result != previous {
+        previous = result
+		result = replaceFirstNumberWord(result)
+	}
+
+	return result
+}
+
+func replaceFirstNumberWord(line string) string {
+	indexes := make([]int, 0)
+	indexToWordLookup := make(map[int]string)
+	result := line
+
+	for k := range wordToDigitMap {
+		idx := strings.Index(line, k)
+		if idx == -1 {
+			continue
+		}
+		indexToWordLookup[idx] = k
+		indexes = append(indexes, idx)
+	}
+
+	if len(indexes) == 0 {
+		return result
+	}
+
+	slices.Sort(indexes)
+	firstWord := indexToWordLookup[indexes[0]]
+	result = strings.Replace(result, firstWord, string(wordToDigitMap[firstWord]), 1)
+
+	return result
+}
+
+func findFirstDigit(line string) (rune, error) {
+	for _, char := range line {
+		switch char {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			return char, nil
+		}
+	}
+
+	return 0, errors.New("no digit found:" + line)
+}
+
+func findLastDigit(line string) (rune, error) {
+	for i := len(line) - 1; i >= 0; i-- {
+		switch line[i] {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			return rune(line[i]), nil
+		}
+	}
+
+	return 0, errors.New("no last digit found")
 }
 
 func sumCoords(coords []string) int {
@@ -67,5 +144,6 @@ func sumCoords(coords []string) int {
 		}
 		result += val
 	}
+
 	return result
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/neilfenwick/advent-of-code
 
-go 1.17
+go 1.21
