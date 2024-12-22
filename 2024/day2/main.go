c475f5cbc660946a7444a78794d1e2ff4a40a386
Neil Fenwick <neil.fenwick@gmail.com>
2024-12-22 15:37:31 +0000

    2024 day 2 part 2

--- 2024/day2/main.go
+++ 2024/day2/main.go
@@ -29,15 +29,17 @@ func main() {
 	}(file)
 
 	reports := parseReports(file)
-	analyzeReports(reports)
+	analyzeReports(reports, undampedReportAnaylyzer)
+	analyzeReports(reports, dampedReportAnaylyzer)
 }
+
+type reportAnalyzer func([]int) bool
 
 func parseReports(file *os.File) [][]int {
 	reports := make([][]int, 0, 1000)
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		// Split the line into words (numbers)
 		words := strings.Fields(scanner.Text())
 		var report []int
 
@@ -56,33 +58,37 @@ func parseReports(file *os.File) [][]int {
 	return reports
 }
 
-func analyzeReports(reports [][]int) {
+func analyzeReports(reports [][]int, analyzer reportAnalyzer) {
 	safeCount := 0
 
-	for line, report := range reports {
-		stableIncreasing := true
-		stableDecreasing := true
-
-		for i := 1; i < len(report); i++ {
-			levelDelta := report[i] - report[i-1]
-			switch {
-			case abs(levelDelta) < 1 || abs(levelDelta) > 3:
-				stableIncreasing = false
-				stableDecreasing = false
-			case levelDelta < 0:
-				stableIncreasing = false
-			default:
-				stableDecreasing = false
-			}
-		}
-
-		if stableIncreasing || stableDecreasing {
+	for _, report := range reports {
+		if analyzer(report) {
 			safeCount++
-			fmt.Printf("Safe report on line %d: %v \n", line+1, report)
 		}
 	}
 
 	fmt.Printf("Safe reports: %d\n", safeCount)
+}
+
+func undampedReportAnaylyzer(report []int) bool {
+	stableIncreasing := true
+	stableDecreasing := true
+
+	for i := 1; i < len(report); i++ {
+		levelDelta := report[i] - report[i-1]
+
+		switch {
+		case abs(levelDelta) < 1 || abs(levelDelta) > 3:
+			stableIncreasing = false
+			stableDecreasing = false
+		case levelDelta < 0:
+			stableIncreasing = false
+		default:
+			stableDecreasing = false
+		}
+	}
+
+	return stableIncreasing || stableDecreasing
 }
 
 func abs(x int) int {
@@ -90,4 +96,22 @@ func abs(x int) int {
 		return -x
 	}
 	return x
+}
+
+func dampedReportAnaylyzer(report []int) bool {
+	if undampedReportAnaylyzer(report) {
+		return true
+	}
+
+	// Try removing one element at a time to test if the report is is valid without it
+	for i := 0; i < len(report); i++ {
+		newReport := make([]int, len(report)-1)
+		copy(newReport, report[:i])
+		copy(newReport[i:], report[i+1:])
+		if undampedReportAnaylyzer(newReport) {
+			return true
+		}
+	}
+
+	return false
 }
