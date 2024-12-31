99ed51389772a82709a87ef6257c616e9afefd68
Neil Fenwick <neil.fenwick@gmail.com>
2024-12-31 16:00:05 +0000

    2024 day 4 part 2

--- 2024/day4/main.go
+++ 2024/day4/main.go
@@ -18,6 +18,9 @@ func main() {
 	mapData := parseMap(file)
 	wordCount := countWords(mapData, []rune{'X', 'M', 'A', 'S'})
 	log.Printf("Word count: %d", wordCount)
+
+	xmasCount := countX(mapData, []rune{'M', 'A', 'S'})
+	log.Printf("XMAS count: %d", xmasCount)
 }
 
 type point struct {
@@ -79,4 +82,47 @@ func checkWord(mapData map[point]rune, start point, searchWord []rune, direction
 		}
 	}
 	return true
+}
+
+func countX(mapData map[point]rune, searchWord []rune) int {
+	xCount := 0
+	var middle int = len(searchWord) / 2 //  "Rounds" down beause division truncates to int
+	if len(searchWord) == 0 {
+		log.Println("Search word must have an odd number of characters to search for X's")
+	}
+
+	// Find all the upleft and downright diagonals that contain the search word
+	// Store the location of the middle rune
+	backdiagonals := make(map[point]rune)
+	for loc, char := range mapData {
+		if char == searchWord[0] {
+			for _, direction := range []vector{upLeft, downRight} {
+				if checkWord(mapData, loc, searchWord, direction) {
+					backdiagonals[point{loc.x + direction.x*middle, loc.y + direction.y*middle}] = char
+				}
+			}
+		}
+	}
+
+	// Find all of the upRight and downLeft diagonals that contain the search word
+	// Store the location of the middle rune
+	diagonals := make(map[point]rune)
+	for loc, char := range mapData {
+		if char == searchWord[0] {
+			for _, direction := range []vector{upRight, downLeft} {
+				if checkWord(mapData, loc, searchWord, direction) {
+					diagonals[point{loc.x + direction.x*middle, loc.y + direction.y*middle}] = char
+				}
+			}
+		}
+	}
+
+	// Cross reference the two sets to find where the middle rune in the search word is in both sets
+	for loc := range backdiagonals {
+		if _, ok := diagonals[loc]; ok {
+			xCount++
+		}
+	}
+
+	return xCount
 }
